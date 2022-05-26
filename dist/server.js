40f7c430d052c0e8092fc05c9d6207f8e9a24048
tandribs <thais.ribeirodeandrade@gmail.com>
2022-05-26 17:58:36 -0300

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,8 @@
+"use strict";
+const person = "Thais";
+console.log(person);
+const sayHello = (name) => {
+    console.log(`Hello ${name}`);
+};
+sayHello("Thais");
+//# sourceMappingURL=server.js.map
